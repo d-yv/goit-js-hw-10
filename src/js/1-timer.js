625125a33858f43eb3182ff53d3d70ff3c864a40
d-yv <yuri.dubrovin@gmail.com>
2025-05-19 01:26:13 +0300

    add function timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,15 +4,53 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+const button = document.querySelector('button');
+button.setAttribute('disabled', '');
+let userSelectedDate = 0;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const dateNow = new Date();
+    let dateVerify = selectedDates[0];
+    if (dateVerify > dateNow) {
+      userSelectedDate = dateVerify;
+      button.removeAttribute('disabled');
+      console.log('userSelectedDate = ', userSelectedDate);
+      console.log('differenceDate = ', convertMs(userSelectedDate - dateNow));
+    } else {
+      iziToast.show({
+        title: 'Error',
+        titleColor: 'red',
+        message: 'Please choose a date in the future',
+        position: 'topCenter',
+      });
+    }
   },
 };
+
+flatpickr('#datetime-picker', options);
+
+button.addEventListener('click', timer);
+
+function timer() {
+  button.setAttribute('disabled', '');
+  const intervalId = setInterval(() => {
+    const now = new Date();
+    const difference = userSelectedDate - now;
+
+    if (difference <= 0) {
+      clearInterval(intervalId);
+    } else {
+      const time = convertMs(difference);
+      console.log(time); //{days: 1, hours: 23, minutes: 59, seconds: 23}
+    }
+  }, 1000);
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -32,7 +70,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
