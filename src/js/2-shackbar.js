09a050e5865b9c376c73c00c361018c7126cca3f
d-yv <yuri.dubrovin@gmail.com>
2025-05-22 21:32:37 +0300

    add shackbar

--- src/js/2-shackbar.js
+++ src/js/2-shackbar.js
@@ -1,2 +1,46 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const submitData = {};
+const createSubmitData = e => {
+  e.preventDefault();
+  const form = e.target.elements;
+  const delay = form.delay.value.trim();
+  const state = form.state.value;
+  submitData.delay = delay;
+  submitData.state = state;
+  e.target.reset();
+
+  const messageData = {
+    position: 'topRight',
+  };
+
+  const snackbar = new Promise((resolve, reject) => {
+    const { state, delay } = submitData;
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve('SUCCESS');
+        messageData.title = 'Succsess';
+        messageData.titleColor = 'green';
+        messageData.message = `✅ Fulfilled promise in ${delay}ms`;
+      } else {
+        reject('ERROR');
+        messageData.title = 'Error';
+        messageData.titleColor = 'red';
+        messageData.message = `❌ Rejected promise in ${delay}ms`;
+      }
+    }, delay);
+  });
+
+  snackbar
+    .then(result => {
+      iziToast.show(messageData);
+    })
+    .catch(error => {
+      iziToast.show(messageData);
+    });
+
+  return console.log(snackbar);
+};
+form.addEventListener('submit', createSubmitData);
